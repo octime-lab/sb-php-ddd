e6a6cbc2e32d47bd8d16353dc3f4562fea8703ef
unknown <i.gernez@octime-group.com>
2019-07-01 16:31:35 +0200

    improved security

--- src/Application/Query/Collection.php
+++ src/Application/Query/Collection.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Application\Query;
 
 use App\Domain\Shared\Bus\Query\Response;
-use App\Domain\Shared\Query\Exception\NotFoundException;
 
 class Collection implements Response
 {
